8049bfac728101665c783df50f4e45ca53405014
kevinnava <kevin.nava00@gmail.com>
2022-01-13 09:38:47 -0600

    day 38 w/ instructions

--- src/day38_Inheritance/carTask/Car.java
+++ src/day38_Inheritance/carTask/Car.java
@@ -43,3 +43,11 @@ public class Car {
                 '}';
     }
 }
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+				add a constructor to set all the fields
+			instance methods:
+				start(), drive(),toString()
+ */
