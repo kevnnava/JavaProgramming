599eaeac874f087bdfb0f062804fb38b39e973bd
kevinnava <kevin.nava00@gmail.com>
2022-01-13 10:03:07 -0600

    day 38

--- src/day38_Inheritance/carTask/Tesla.java
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -1,4 +1,12 @@
 package day38_Inheritance.carTask;
 
-public class Tesla {
+public class Tesla extends Car{
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand + " " + model + " is in autopilot mode");
+    }
 }
