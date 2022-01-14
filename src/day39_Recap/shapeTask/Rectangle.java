595f24283df3708274dff98a9561d8dd119b2796
kevinnava <kevin.nava00@gmail.com>
2022-01-14 14:38:15 -0600

    day 39 pt 5

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -33,6 +33,26 @@ public class Rectangle extends Shape{
         setLength(length);
         setWidth(width);
     }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
 /*
 Rectangle extends Shape:
