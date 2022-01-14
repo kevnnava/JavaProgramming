5eabe47d4fef0a753ce8cc57dad5f3be3f769730
kevinnava <kevin.nava00@gmail.com>
2022-01-14 14:35:32 -0600

    day 39 pt 4

--- src/day39_Recap/shapeTask/Rectangle.java
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -1,6 +1,6 @@
 package day39_Recap.shapeTask;
 
-public class Rectangle {
+public class Rectangle extends Shape{
 
     private double length, width;
 
@@ -26,6 +26,12 @@ public class Rectangle {
             System.exit(1);
         }
         this.width = width;
+    }
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
     }
 }
 /*
