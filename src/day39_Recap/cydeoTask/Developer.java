6497fdeabbccaa5d5a1805e2c2879ceda9fb6b9c
kevinnava <kevin.nava00@gmail.com>
2022-01-15 01:18:04 -0600

    day 39 pt 12

---
+++ src/day39_Recap/cydeoTask/Developer.java
@@ -0,0 +1,12 @@
+package day39_Recap.cydeoTask;
+
+public class Developer {
+
+
+}
+/*
+4. Create a sub class of Employee named Developer
+            Override the work method
+            Extra methods:
+                fixingBugs()
+ */
