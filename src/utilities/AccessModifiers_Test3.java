3f6acfeb8e9445d57ee886e99cb1bc746f7ce5af
kevinnava <kevin.nava00@gmail.com>
2022-01-20 10:43:20 -0600

    day 40 pt 2

--- src/utilities/AccessModifiers_Test3.java
+++ src/utilities/AccessModifiers_Test3.java
@@ -1,9 +1,13 @@
 package utilities;
 
+import day40_FinalKeyword.ProtectedAccessModifier;
+
 public class AccessModifiers_Test3 {
 
     public static void main(String[] args) {
 
+        // System.out.println(ProtectedAccessModifier.name1);
+        // System.out.println(ProtectedAccessModifier.name2);
 
     }
 }
