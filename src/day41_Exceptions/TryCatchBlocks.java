da920a216479a22f55c3193f45a9e7fcdd10d4d6
kevinnava <kevin.nava00@gmail.com>
2022-01-22 13:36:16 -0600

    day 41 pt 3

--- src/day41_Exceptions/TryCatchBlocks.java
+++ src/day41_Exceptions/TryCatchBlocks.java
@@ -1,5 +1,8 @@
 package day41_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class TryCatchBlocks {
 
     public static void main(String[] args) {
@@ -56,9 +59,29 @@ public class TryCatchBlocks {
             e.printStackTrace();
         }
 
-        System.out.println("Test 4 Started");
+        System.out.println("Test 3 Started");
 
         System.out.println("--------------------------");
+
+        System.out.println("Hello");
+
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Cydeo");
+
+        System.out.println("--------------------------");
+
+        try {
+            FileInputStream file = new FileInputStream("File Path");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
