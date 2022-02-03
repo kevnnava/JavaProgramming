234363b9abdc4c95d175adebe6c716b980d6897e
kevinnava <kevin.nava00@gmail.com>
2022-02-03 15:24:22 -0600

    day 44 part 2

--- src/day44_Abstraction/animalTask/Animal.java
+++ src/day44_Abstraction/animalTask/Animal.java
@@ -1,5 +1,14 @@
 package day44_Abstraction.animalTask;
 
+/*
+class Test{
+    public static void main(String[] args) {
+        System.out.println(new Test().getClass().getSimpleName());
+        System.out.println(new Test().getClass().getName());
+    }
+}
+ */
+
 public abstract class Animal {
 
     private String name;
---
+++ src/day44_Abstraction/animalTask/Cat.java
@@ -0,0 +1,17 @@
+package day44_Abstraction.animalTask;
+
+public final class Cat extends Animal{
+
+
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void eat() {
+        System.out.println(getName() + " is eating");
+    }
+
+    public void meow(){
+        System.out.println(getName() + " is meowing");
+    }
+}
