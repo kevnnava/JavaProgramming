72fc3338404cdafe280489eccd782f8c68bd38f0
kevinnava <kevin.nava00@gmail.com>
2022-02-15 07:47:01 -0600

    commit 2

--- src/day44_Abstraction/animalTask/Parrot.java
+++ src/day44_Abstraction/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Parrot extends Animal implements Playable{
+public final class Parrot extends Animal implements Playable, Flyable{
 
 
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
@@ -15,5 +15,10 @@ public final class Parrot extends Animal implements Playable{
     @Override
     public void play() {
 
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " can fly 20km/h");
     }
 }
